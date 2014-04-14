9e027839f438de6a618d6c1512d32335361a6dcc
Antonio Sastre <antonio@sastre.es>
2014-04-14 21:04:30 +0200

    Documentación

--- tabs/notas/element.php
+++ tabs/notas/element.php
@@ -4,7 +4,6 @@ border-width:1px;">
 echo "ID: ".$nota['id']."<br />";
 echo "Editando: ".$nota['isediting']."<br />";
 echo "Prio: ".$nota['prioridad']."<br />";
-echo "Trabajo: ".$nota['trabajo']."<br />";
 echo "In: ".$nota['fecha_in']."<br />";
 echo "Out: ".$nota['fecha_out']."<br />";
 echo "Cliente: ".$nota['cliente']."<br />";
@@ -12,23 +11,25 @@ echo "Nota Clie: ".$nota['notacliente']."<br />";
 echo "Pre: ".$nota['precio']."<br />";
 ?>
 
+<table>
+	<tr>
+		<td><?php echo $nota['trabajo'] ?></td>
+	</tr>
+</table>
+
 <div class="notas-de-nota">
 	<ul>
 		<li><a href="#ndn-<?php echo $nota['id']?>-1">General</a></li>
-		<li><a href="#ndn-<?php echo $nota['id']?>-2">Impresión</a></li>
-		<li><a href="#ndn-<?php echo $nota['id']?>-3">Manipulado</a></li>
-		<li><a href="#ndn-<?php echo $nota['id']?>-4">Entrega</a></li>
+		<li><a href="#ndn-<?php echo $nota['id']?>-2">Cliente</a></li>
+		<li><a href="#ndn-<?php echo $nota['id']?>-3">Entrega</a></li>
 	</ul>
 	<div id="ndn-<?php echo $nota['id']?>-1">
 		<textarea rows="5" cols="56"><?php echo $nota['nota_gen'] ?></textarea>
 	</div>
 	<div id="ndn-<?php echo $nota['id']?>-2">
-		<textarea rows="5" cols="56"><?php echo $nota['nota_imp'] ?></textarea>
+		<textarea rows="5" cols="56"><?php echo $nota['notacliente'] ?></textarea>
 	</div>
 	<div id="ndn-<?php echo $nota['id']?>-3">
-		<textarea rows="5" cols="56"><?php echo $nota['nota_man'] ?></textarea>
-	</div>
-	<div id="ndn-<?php echo $nota['id']?>-4">
 		<textarea rows="5" cols="56"><?php echo $nota['nota_ent'] ?></textarea>
 	</div>
 </div>
